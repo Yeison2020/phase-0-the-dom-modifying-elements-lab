8f6ebe471e8e7921ff5f59e5f4e74f0dfffb078c
Yeison <enmanuelquiterios@gmail.com>
2021-08-27 12:28:37 -0400

    Completed assigment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+
+document.querySelector('h1#victory');
+
+const pContent = document.createElement('p');
+
+pContent.innerText = 'hello'
+
+document.getElementById('main').remove();
+let newHeader; 
+newHeader = document.querySelectorAll('h1#victory')
+
+newHeader.innerHTML = ' Yeison is the champion'
