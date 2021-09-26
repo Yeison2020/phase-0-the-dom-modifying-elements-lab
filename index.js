6d011f8ca25dd68ce697691631bc8dd1b97a7eb0
Yeison <enmanuelquiterios@gmail.com>
2021-09-26 14:03:10 -0400

    commit tree 412daf42ac7b69d9bc52010c3a9192f0907faf33

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 // Write your code here!
 
-document.querySelector('h1#victory');
+// document.querySelector('h1#victory');
 
-const pContent = document.createElement('p');
+// const pContent = document.createElement('p');
 
-pContent.innerText = 'hello'
+// pContent.innerText = 'hello'
 
-document.getElementById('main').remove();
-let newHeader; 
-newHeader = document.querySelectorAll('h1#victory')
+// document.getElementById('main').remove();
+// let newHeader;
+// newHeader = document.querySelectorAll('h1#victory')
 
-newHeader.innerHTML = ' Yeison is the champion'
+// newHeader.innerHTML = ' Yeison is the champion'
+const main = document.querySelector("#main");
+main.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.innerHTML = "Yeison Casado is the champion";
+newHeader.id = "victory";
+document.body.append(newHeader);
+
+console.log(newHeader);
